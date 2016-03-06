78f692638d0654331205f16276d5b7669b82db78
Andrey <andrey@NOTE.zar>
2016-03-05 17:28:08 -0800

    New samples

---
+++ samples/list-editor-rest-api/sample_api.php
@@ -0,0 +1,71 @@
+<?php
+
+class SampleAPI extends API {
+    
+    /**
+     * DELETE
+     * @return string
+     */
+    public function _delete() {
+        
+        $id = (int)$this->id;
+        if (!DB::getInstance()->exist($id)) {
+            return $this->response(array('error' => "Product not exist"), 404);
+        }
+        
+        DB::getInstance()->delete($id);
+        
+        return $this->response();
+    }
+    
+    /**
+     * CREATE
+     * @return string
+     */
+    public function _post() {
+        
+        $newProduct = array(
+            'name' => ($this->name ? $this->name : ''),
+            'category' => ($this->category ? $this->category : ''),
+            'delivered' => false,
+        );
+        
+        $newProduct['id'] = DB::getInstance()->add($newProduct);
+        
+        return $this->response($newProduct);
+    }
+    
+    /**
+     * GET LIST
+     * @return string
+     */
+    public function _get() {
+        return $this->response( DB::getInstance()->get() );
+    }
+    
+    /**
+     * UPDATE
+     * @return string
+     */
+    public function _put() {
+        
+        $id = (int)$this->id;
+        if (!DB::getInstance()->exist($id)) {
+            return $this->response(array('error' => "Product not exist"), 404);
+        }
+        
+        $newData = array(
+            'name' => ($this->name ? $this->name : ''),
+            'category' => ($this->category ? $this->category : ''),
+            'delivered' => (bool)($this->delivered ? $this->delivered : false),
+        );
+        
+        DB::getInstance()->change($id, $newData);
+        
+        $newData['id'] = $id;
+        
+        return $this->response($newData);
+    }
+    
+}
+
--- samples/list-editor-with-rest-api/app.js
+++ samples/list-editor-with-rest-api/app.js
@@ -1,7 +1,7 @@
 
 var applicationUrl = "list-editor-with-rest-api/";
 var viewsUrl = applicationUrl + "views/";
-var apiUrl = applicationUrl + "rest/products.php";
+var apiUrl = "list-editor-rest-api/products.php";
 var scriptFileName = "/list-editor-with-rest-api.html";
 
 var listEditorApp = angular.module("ListEditorApp", ["ngRoute"]);
